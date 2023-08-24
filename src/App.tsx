4034417381f8883226e2eb330c1e66ce10eaef9c
darko.tomicic <dtomicic192@gmail.com>
2023-08-24 11:37:38 +0200

    started creating a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
-     <Navigation />
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
